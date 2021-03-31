65d43f36bcf43f77849b090f7021edf69c626371
jaden <jonghaha7@gmail.com>
2021-03-31 19:55:53 +0900

    Initial commit

---
+++ src/main/kotlin/com/depromeet/webtoon/WebtoonApplication.kt
@@ -0,0 +1,11 @@
+package com.depromeet.webtoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebtoonApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebtoonApplication>(*args)
+}
