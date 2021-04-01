dd19677fed2e0b38ccf0bab216cb134407a1bdd9
devsejong <sejong.park@woowahan.com>
2021-04-01 14:14:09 +0900

    kotest 의존관계 추가

--- src/test/kotlin/com/depromeet/webtoon/domain/webtoon/WebtoonRepositoryOldTest.kt
+++ src/test/kotlin/com/depromeet/webtoon/domain/webtoon/WebtoonRepositoryOldTest.kt
@@ -1,23 +1,23 @@
 package com.depromeet.webtoon.domain.webtoon
 
-import io.kotest.core.spec.style.FunSpec
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class WebtoonRepositoryTest constructor(
-    var webtoonRepository: WebtoonRepository
-) : FunSpec({
+class WebtoonRepositoryOldTest @Autowired constructor(val webtoonRepository: WebtoonRepository) {
 
-    test("Webtoon 생성 및 반환 확인") {
+    @Test
+    fun 생성_조회_테스트() {
         // given
         val testWebtoon = Webtoon("웹툰")
 
         // when
-        val savedWebtoon = webtoonRepository.saveAndFlush(testWebtoon)
+        val savedWebtoon = webtoonRepository.save(testWebtoon)
 
         // then
         val foundWebtoon = webtoonRepository.getOne(savedWebtoon.id!!)
         assertThat(foundWebtoon.id).isNotNull()
     }
-})
+}
