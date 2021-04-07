e898cf37903dcd5acdad8f8f618144b63b49ce2a
devsejong <sejong.park@woowahan.com>
2021-04-07 20:24:20 +0900

    프로젝트 구조 설정

--- webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/WebtoonApiApplication.kt
+++ webtoon-api/src/main/kotlin/com/depromeet/webtoon/api/WebtoonApiApplication.kt
@@ -3,10 +3,8 @@ package com.depromeet.webtoon.api
 import com.depromeet.webtoon.core.WebtoonCoreRoot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
-@SpringBootApplication
-@Import(WebtoonCoreRoot::class)
+@SpringBootApplication(scanBasePackageClasses = [WebtoonApiApplication::class, WebtoonCoreRoot::class])
 class WebtoonApiApplication
 
 fun main(args: Array<String>) {
